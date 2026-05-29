dfbb7bd637c5c21780946793ad8001ea02fc9d84
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:16:39 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,8 @@
+def juft_toq():
+    son = int(input("Istalgan sonni kiriting: "))
+    if son % 2 == 0:
+        print("Kiritilgan son juft.")
+    else:
+        print("Kiritilgan son toq.")
+
+juft_toq()
